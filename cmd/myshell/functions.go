5fd5c0ba860fffc500c8eab76e7ab5074b682d26
OptimusPrimuser <rahulkv3jpr@gmail.com>
2024-06-08 16:38:53 +0530

    pass stage

--- cmd/myshell/functions.go
+++ cmd/myshell/functions.go
@@ -85,6 +85,7 @@ func typeFunc(args []string) {
 }
 
 func executeExternal(args []string, execPath string) {
+	fmt.Println(execPath)
 	cmdArray := append([]string{execPath}, args...)
 	cmd := exec.Command("/bin/sh", cmdArray...)
 	err := cmd.Run()
